18cf0b0eb961287bc6fa2114ea90ddd24fb49e5c
h136479442@gmail.com <h136479442@gmail.com>
2020-10-28 15:00:06 +0900

    fixed templete and add js-file

---
+++ script.js
@@ -0,0 +1,27 @@
+$(function () {
+  //ハンバーガーメニュー
+  $(".btn__hamburger").click(function () {
+    $(this).toggleClass("active");
+
+    if ($(this).hasClass("active")) {
+      $(".hamburger").addClass("active");
+    } else {
+      $(".hamburger").removeClass("active");
+    }
+  });
+  $(".hamburger__list").click(function () {
+    $(".hamburger,.btn__hamburger").removeClass("active");
+  });
+
+  //ヘッダー固定(SP)
+  //fvを超えたらスクロールでheaderに色を付ける
+  var mainPos = $(".fv").height();
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > mainPos) {
+      $(".header__inner").addClass("addColor");
+    } else {
+      $(".header__inner").removeClass("addColor");
+    }
+  });
+});
